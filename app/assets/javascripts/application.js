a13fec77fa66f21d1c7187a39e651edd0fd7974a
Mitali Mulgaonkar <mitali.mulgaonkar@presentsoft.co.in>
2017-05-25 15:02:57 +0530

    Mitali: allowing only digits for number of axle field

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,4 +28,12 @@ $(document).ready(function() {
 
     });
 
+    //allow only digits for number of axle field
+    $("#axle").keypress(function (e) {
+        //if the letter is not digit then display error and don't type anything
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            $("#errmsg").html("Digits Only").show().fadeOut("slow");
+            return false;
+        }
+    });
 });
